98c941654288d6d0004316ed64cfd8f4e4658f4f
Caedwin <edwin.a.camacho@gmail.com>
2022-04-30 09:23:58 +0100

    creando archivos en git

---
+++ js/contador.js
@@ -0,0 +1,11 @@
+
+//función contador.js
+
+var longitud = document.querySelector("#cuerpo-mensaje");
+
+longitud.addEventListener("input",function(){
+console.log(longitud);
+ 
+
+return longitud.textContent;
+});
